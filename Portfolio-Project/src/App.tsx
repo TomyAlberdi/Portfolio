987fb0ad628b8d1e8e08bf5615cfc126316d82b0
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2025-04-08 00:54:41 -0300

    started progress bar implementation

--- Portfolio-Project/src/App.tsx
+++ Portfolio-Project/src/App.tsx
@@ -1,10 +1,49 @@
 import "@/index.css";
+import { useEffect, useState } from "react";
+
+const sections = [
+  "about",
+  "personal-projects",
+  "work-experience",
+  "skills",
+  "contact",
+];
 
 function App() {
+  const [Progress, setProgress] = useState<number>(0);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const index = sections.indexOf(entry.target.id);
+            if (index !== -1) {
+              setProgress((index / (sections.length - 1)) * 100);
+            }
+          }
+        });
+      },
+      { threshold: 0.6 }
+    );
+    sections.forEach((id) => {
+      const element = document.getElementById(id);
+      if (element) observer.observe(element);
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <div className="App min-h-screen text-background">
-      <section className="min-h-screen backdrop-blur-xs"> 
-        test
+      <section className="min-h-screen backdrop-blur-xs flex justify-center items-start">
+        <div className="h-full w-full md:w-1/2 flex justify-center items-center">
+          <div
+            className="bg-blue-500 w-1 transition-all duration-300"
+            style={{ height: `${Progress}` }}
+          />
+          <main className="w-full p-4 border border-red-500">sss</main>
+        </div>
       </section>
     </div>
   );
