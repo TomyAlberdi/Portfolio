ae062cabdd868f8d096ee0b7912e9f831ec8eb73
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-05-25 23:50:58 -0300

    minimal project component setup

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,14 +1,16 @@
 import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { useTranslation } from "react-i18next";
 
 const Projects = () => {
   const { t } = useTranslation();
   //TODO: Design & implement Personal projects component
   return (
-    <Card id="projects" className="w-full">
-      <CardHeader>
+    <Card id="projects" className="w-full flex-row pt-0">
+      <CardHeader className="w-1/2 pt-6">
         <CardTitle>{t("Projects")}</CardTitle>
       </CardHeader>
+      <ScrollArea className="border border-red-500 w-1/2 max-h-[20svh]"></ScrollArea>
     </Card>
   );
 };
